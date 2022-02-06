868fb5bc5d29963f94b29f704a737b1da8c4bf8a
Markus Nickels <MarkusNickels@mac.com>
2022-02-06 18:09:45 +0100

    Daily commit

--- src/rr_Intervall.h
+++ src/rr_Intervall.h
@@ -9,11 +9,9 @@
 //!
 //! This file is part of the Application "rr_ArduinoUtils".
 //!
-//! This work is licensed under the
+//!      Creative Commons Attribution-ShareAlike 4.0 International License.
 //!
-//!      Creative Commons Attribution-NonCommercial 4.0 International License.
-//!
-//! To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/
+//! To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
 //! or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 //!
 
