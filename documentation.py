56ddbcd448a1b73eecaa3f3d1314699c6d5f7e09
Markus Nickels <MarkusNickels@mac.com>
2022-02-19 21:04:59 +0100

    Extract documenation information

--- documentation.py
+++ documentation.py
@@ -17,14 +17,42 @@
 
 import gitVersion as gitVersion
 import os
+import json
+
 
 # generate documentation
 
 
 def generate(source, target, env):
     if os.path.isfile(os.path.join(env['PROJECT_DIR'], "Doxyfile")):
-        command = "(cat Doxyfile; echo \"PROJECT_NUMBER=" + \
-            gitVersion.short() + "\" ) | doxygen -"
+        template = "library-template.json"
+
+        projectBrief = ""
+        projectName = ""
+        projectNumber = gitVersion.short()
+
+        # extract information from libary-template.json
+        if os.path.isfile(template):
+            with open(template, 'r') as f:
+                data = json.load(f)
+
+            if "name" in data:
+                projectName = data["name"]
+
+            if "description" in data:
+                projectBrief = data["description"]
+
+        # feed values into doxygen
+        command = "(cat Doxyfile; "
+        command = command + "echo \"PROJECT_NUMBER=" + projectNumber + "\";"
+
+        if projectBrief:
+            command = command + "echo \"PROJECT_BRIEF=" + projectBrief + "\";"
+
+        if projectName:
+            command = command + "echo \"PROJECT_NAME=" + projectName + "\";"
+
+        command = command + " ) | doxygen -"
 
         print("Creating documentation ...")
 
